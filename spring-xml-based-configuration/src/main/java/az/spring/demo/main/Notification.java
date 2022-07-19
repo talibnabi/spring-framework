f8e741679419addc76d3cd1bf91a5ebbc4f00547
talibnabi <talibnabiyev@gmail.com>
2022-07-19 05:32:19 -0700

    spring

--- spring-xml-based-configuration/src/main/java/az/spring/demo/main/Notification.java
+++ spring-xml-based-configuration/src/main/java/az/spring/demo/main/Notification.java
@@ -1,8 +1,13 @@
 package az.spring.demo.main;
 
 import az.spring.demo.impl.Email;
+import az.spring.demo.impl.Employee;
+import az.spring.demo.impl.Hammer;
+import az.spring.demo.inter.Device;
 import az.spring.demo.inter.Human;
 import az.spring.demo.inter.Message;
+
+import java.util.ArrayList;
 
 public class Notification {
     private Message message;
@@ -35,6 +40,13 @@ public class Notification {
     public void alert() {
         System.out.println("Notifications......");
         message.send();
+        Employee human = (Employee) this.employee;
+        Hammer hammer1 = (Hammer) human.hammers.get(0);
+        hammer1.name = "hammerZor";
+        hammer1.version = 999;
+        human.hammers.add(new Hammer("first", 1));
+        human.hammers.add(new Hammer("second", 2));
+        human.hammers.add(new Hammer("third", 3));
         System.out.println(employee.toString());
         employee.send();
     }
