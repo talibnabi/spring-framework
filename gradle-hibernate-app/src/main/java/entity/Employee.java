82e63f85389697190fdb8d260b9ad32232c8990f
talibnabi <talibnabiyev@gmail.com>
2022-08-06 05:19:47 -0700

    spring lesson

--- gradle-hibernate-app/src/main/java/entity/Employee.java
+++ gradle-hibernate-app/src/main/java/entity/Employee.java
@@ -1,6 +1,25 @@
 package entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
 public class Employee {
-    private Integer id;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer employeeId;
+
+    @Column(name = "name")
+    private String employeeName;
+
+    @Column(name = "salary")
+    private Integer employeeSalary;
+
 }
---
+++ gradle-hibernate-app/src/main/java/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        Configuration cfg = new Configuration().configure();
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Employee employee = new Employee();
+        employee.setEmployeeName("Alisa");
+        employee.setEmployeeSalary(19000);
+
+        session.saveOrUpdate(employee);
+        transaction.commit();
+        session.close();
+    }
+}
