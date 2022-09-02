4bd64b9a949659149081dd8617c259fca53a47d7
talibnabi <talibnabiyev@gmail.com>
2022-09-02 03:45:34 -0700

    commit

--- spring-boot-tutorial/src/main/java/com/company/springboottutorial/entity/Department.java
+++ spring-boot-tutorial/src/main/java/com/company/springboottutorial/entity/Department.java
@@ -5,17 +5,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(
+        name = "tbl_department",
+        uniqueConstraints = @UniqueConstraint(
+                name = "department_code_constraint",
+                columnNames = "department_code"
+        )
+)
 public class Department {
+    @SequenceGenerator(
+            name = "doctor_sequence",
+            sequenceName = "doctor_sequence",
+            allocationSize = 1
+    )
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "doctor_sequence"
+    )
     @Column(name = "department_id")
     private Long departmentId;
     @Column(name = "department_name")
