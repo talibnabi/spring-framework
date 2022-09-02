f9958c5ff1ee4ff2dbca5957c36b00a7725da3ba
talibnabi <talibnabiyev@gmail.com>
2022-09-02 02:58:42 -0700

    commit

---
+++ spring-boot-tutorial/src/main/java/com/company/springboottutorial/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.company.springboottutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello() {
+        return "hello,dear";
+    }
+//    @GetMapping("/")
+//    public String hello() {
+//        return "hello,dear";
+//    }
+}
