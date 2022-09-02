a4ec69975a0e3262d7f8fd0ca0c1f88ea92a88e2
talibnabi <talibnabiyev@gmail.com>
2022-09-02 04:16:07 -0700

    commit

--- spring-boot-tutorial/src/main/java/com/company/springboottutorial/controller/DepartmentController.java
+++ spring-boot-tutorial/src/main/java/com/company/springboottutorial/controller/DepartmentController.java
@@ -1,8 +1,16 @@
 package com.company.springboottutorial.controller;
 
+import com.company.springboottutorial.service.inter.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/department")
+@RestController
+@RequestMapping("/department")
+@RequiredArgsConstructor
+@CrossOrigin
 public class DepartmentController {
-    
+
+    private DepartmentService departmentService;
 }
