2601d2bf6de22bb3f03330775894f75b213e5137
talibnabi <talibnabiyev@gmail.com>
2022-09-03 07:18:39 -0700

    commit

---
+++ spring-boot-tutorial/src/main/java/com/company/springboottutorial/error/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.company.springboottutorial.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    
+}
