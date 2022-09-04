1df74c981cefccb721377dbf28e1524618cb5159
talibnabi <talibnabiyev@gmail.com>
2022-09-04 07:54:49 -0700

    commit

--- spring-boot-tutorial/src/test/java/com/company/springboottutorial/service/impl/DepartmentServiceImplTest.java
+++ spring-boot-tutorial/src/test/java/com/company/springboottutorial/service/impl/DepartmentServiceImplTest.java
@@ -1,14 +1,24 @@
 package com.company.springboottutorial.service.impl;
 
+import com.company.springboottutorial.service.inter.DepartmentService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
+@RequiredArgsConstructor
 class DepartmentServiceImplTest {
+    private final DepartmentService departmentService;
 
     @BeforeEach
     void setUp() {
 
+    }
+
+    @Test
+    public void findDepartmentByDepartmentNameTest() {
+        
     }
 }
