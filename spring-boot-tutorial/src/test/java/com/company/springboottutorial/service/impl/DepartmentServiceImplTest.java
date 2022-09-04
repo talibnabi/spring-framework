ce5619efa40053e564e7668c32c594fc9a6a6cd2
talibnabi <talibnabiyev@gmail.com>
2022-09-04 07:52:43 -0700

    commit

---
+++ spring-boot-tutorial/src/test/java/com/company/springboottutorial/service/impl/DepartmentServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.company.springboottutorial.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class DepartmentServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}
