8b9efa2127c80885bb1a2196043c825258263210
talibnabi <talibnabiyev@gmail.com>
2022-09-05 03:00:43 -0700

    commit

--- spring-boot-tutorial/src/test/java/com/company/springboottutorial/repository/DepartmentRepositoryTest.java
+++ spring-boot-tutorial/src/test/java/com/company/springboottutorial/repository/DepartmentRepositoryTest.java
@@ -1,7 +1,28 @@
 package com.company.springboottutorial.repository;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class DepartmentRepositoryTest {
-    
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @BeforeEach
+    void setUp() {
+        
+    }
+
+    @Test
+    public void findDepartmentByDepartmentNameTest() {
+
+    }
+
 }
