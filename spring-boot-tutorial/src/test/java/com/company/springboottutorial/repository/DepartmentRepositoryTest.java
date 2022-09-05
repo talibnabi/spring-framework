d2d34c86f02306068e974d92994e7e7af6a05781
talibnabi <talibnabiyev@gmail.com>
2022-09-05 04:09:08 -0700

    commit

--- spring-boot-tutorial/src/test/java/com/company/springboottutorial/repository/DepartmentRepositoryTest.java
+++ spring-boot-tutorial/src/test/java/com/company/springboottutorial/repository/DepartmentRepositoryTest.java
@@ -1,34 +1,27 @@
 package com.company.springboottutorial.repository;
 
+
 import com.company.springboottutorial.entity.Department;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class DepartmentRepositoryTest {
     @Autowired
     private DepartmentRepository departmentRepository;
     @Autowired
-    private TestEntityManager testEntityManager;
+    private TestEntityManager entityManager;
 
     @BeforeEach
     void setUp() {
         Department department = Department.builder()
-                .departmentName("Silisium")
-                .departmentAddress("Gabala")
-                .departmentCode("12345")
+                .departmentName("Mechanical engineering")
+                .departmentCode("me-7")
+                .departmentAddress("Baku")
                 .build();
-        testEntityManager.persist(department);
-    }
-
-    @Test
-    public void findDepartmentByDepartmentNameTest() {
-        
+        entityManager.persist(department);
     }
 
 }
