720dc532ffcc3712672f262a1fa3c7f9549afef6
talibnabi <talibnabiyev@gmail.com>
2022-09-07 10:18:32 -0700

    commit

--- spring-boot-master-class/src/main/java/com/company/springbootmasterclass/demo/Main.java
+++ spring-boot-master-class/src/main/java/com/company/springbootmasterclass/demo/Main.java
@@ -1,8 +1,13 @@
 package com.company.springbootmasterclass.demo;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext();
         Doctor doctor = new Doctor();
         doctor.assist();
     }
