2ed87774db69def6229a1c040f205118d1051e03
talibnabi <talibnabiyev@gmail.com>
2023-01-03 05:14:01 -0800

    commit

---
+++ abb-spring-seventh/src/main/java/com/company/springseventh/model/Extra.java
@@ -0,0 +1,20 @@
+package com.company.springseventh.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "extra")
+public class Extra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "x_id")
+    private Long id;
+
+    @Column(name = "x_info")
+    private String info;
+
+}
---
+++ abb-spring-seventh/src/main/java/com/company/springseventh/model/Person.java
@@ -0,0 +1,25 @@
+package com.company.springseventh.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "extra_id", referencedColumnName = "x_id")
+    private Extra extra;
+
+}
